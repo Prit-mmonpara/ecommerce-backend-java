7395b3e55d9bfbe709e32a4346edb156f5bb540d
Prit-mmonpara <115855690+Prit-mmonpara@users.noreply.github.com>
2025-06-24 22:34:55 +0530

    Delete ecommerce directory

--- ecommerce/src/main/java/com/ecommerce/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.repository;
-
-import com.ecommerce.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    Order findByRazorpayOrderId(String razorpayOrderId);
-}
